5953f44c476b14366eda9238943061b1ab30f64a
Brian Pepe <brian.pepe@derivesystems.com>
2019-10-30 15:26:09 -0400

    Fixed issue where methods with multiple parameters were not working. Added server support for nested interfaces. Only one level deep.

--- InterfaceRpc.Service/RouteManager.cs
+++ InterfaceRpc.Service/RouteManager.cs
@@ -9,34 +9,35 @@ using SerializerDotNet;
 
 namespace InterfaceRpc.Service
 {
-	internal class RouteManager<T> where T : class
-	{
-		private readonly MethodInfo[] _interfaceMethods;
-		private readonly Type _interfaceType;
-		private readonly IDictionary<string, Func<string, T, HttpContext, Task<RouteResponse>>> _routeHandlers;
+    internal class RouteManager<T> where T : class
+    {
+        private readonly MethodInfo[] _interfaceMethods;
+        private readonly Type _interfaceType;
+        private readonly IDictionary<string, Func<string, T, HttpContext, Task<RouteResponse>>> _routeHandlers;
         private readonly RpcServiceOptions<T> _options;
 
-		private static readonly MethodInfo _getRequestEntityMethod = typeof(SerializationHelper).GetMethod("GetRequestEntityAsync", BindingFlags.Static | BindingFlags.NonPublic);
+        private static readonly MethodInfo _getRequestEntityMethod = typeof(SerializationHelper).GetMethod("GetRequestEntityAsync", BindingFlags.Static | BindingFlags.NonPublic);
 
-		public RouteManager(RpcServiceOptions<T> options)
-		{
-			_interfaceType = typeof(T);
-			_interfaceMethods = _interfaceType.GetMethods();
-			_routeHandlers = new Dictionary<string, Func<string, T, HttpContext, Task<RouteResponse>>>();
+        public RouteManager(RpcServiceOptions<T> options)
+        {
+            _interfaceType = typeof(T);
+            _interfaceMethods = GetAllMethods(_interfaceType);
+            _routeHandlers = new Dictionary<string, Func<string, T, HttpContext, Task<RouteResponse>>>();
             _options = options ?? throw new ArgumentNullException(nameof(options));
 
-			foreach (var method in _interfaceMethods)
-			{
-				AddHandler(method.Name, MethodHandler);
-			}
-		}
+            foreach (var method in _interfaceMethods)
+            {
+                AddHandler(method.Name, MethodHandler);
+            }
+        }
 
-		private async Task<RouteResponse> MethodHandler(string methodName, T instance, HttpContext context)
-		{
+
+        private async Task<RouteResponse> MethodHandler(string methodName, T instance, HttpContext context)
+        {
             var method = _interfaceMethods.FirstOrDefault(x => x.Name.Equals(methodName, StringComparison.OrdinalIgnoreCase));
-			if (method == null)
-			{
-				throw new NullReferenceException(string.Format("Could not find method corresponding to {0}", methodName));
+            if (method == null)
+            {
+                throw new NullReferenceException(string.Format("Could not find method corresponding to {0}", methodName));
             }
 
             var contentType = SerializationHelper.GetContentType(context.Request);
@@ -66,7 +67,7 @@ namespace InterfaceRpc.Service
             }
 
             var parameterValues = new List<object>();
-			var methodParameters = method.GetParameters();
+            var methodParameters = method.GetParameters();
             var serializer = Serializer.GetSerializerFor(contentType);
 
             if (methodParameters.Any())
@@ -90,11 +91,11 @@ namespace InterfaceRpc.Service
                     await task.ConfigureAwait(false);
                     var resultProperty = task.GetType().GetProperty("Result");
                     entity = resultProperty.GetValue(task);
-
+                    parameterValues.AddRange(TupleToEnumerable(entity));
                 }
             }
 
-                var result = method.Invoke(instance, parameterValues.ToArray());
+            var result = method.Invoke(instance, parameterValues.ToArray());
             var isTask = method.ReturnType == typeof(Task);
 
             if (method.ReturnType == typeof(Task))
@@ -102,96 +103,131 @@ namespace InterfaceRpc.Service
                 await (Task)result;
                 return response;
             }
-            else if(method.ReturnType.IsGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
+            else if (method.ReturnType.IsGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
             {
                 result = await (dynamic)result;
             }
 
             if (method.ReturnType != typeof(void))
-			{
-				if (result != null)
-				{
-					response.Content = await serializer.SerializeAsync(result);
-				}
-				else
-				{
-					response.Content = new byte[0];
-				}
-			}
-			return response;
-		}
+            {
+                if (result != null)
+                {
+                    response.Content = await serializer.SerializeAsync(result);
+                }
+                else
+                {
+                    response.Content = new byte[0];
+                }
+            }
+            return response;
+        }
 
-		private static IEnumerable<object> TupleToEnumerable(object tuple)
-		{
-			var tupleType = tuple.GetType();
-			var tupleTypeProperties = tupleType.GetRuntimeFields();
-			var values = tupleTypeProperties.Select(x => x.GetValue(tuple));
-			return values;
-		}
+        private static IEnumerable<object> TupleToEnumerable(object tuple)
+        {
+            var tupleType = tuple.GetType();
+            var tupleTypeProperties = tupleType.GetRuntimeFields();
+            var values = tupleTypeProperties.Select(x => x.GetValue(tuple));
+            return values;
+        }
 
-		public static Type GetTupleType(Type[] types)
-		{
-			var valueTupleCreateMethod = typeof(ValueTuple).GetMethods().FirstOrDefault(x => x.Name == "Create" && x.GetParameters().Count() == types.Length);
-			if (valueTupleCreateMethod == null)
-			{
-				//could happen if there are more than 8 arguments
-				throw new ApplicationException("Cannot deserialize this method's arguments. Try cutting the number of arguments down to 8 or less.");
-			}
-			var genericValueTupleCreateMethod = valueTupleCreateMethod.MakeGenericMethod(types);
-			var dummyValues = new List<object>();
-			foreach (var type in types)
-			{
-				if (type.IsValueType)
-				{
-					dummyValues.Add(Activator.CreateInstance(type));
+        public static Type GetTupleType(Type[] types)
+        {
+            var valueTupleCreateMethod = typeof(ValueTuple).GetMethods().FirstOrDefault(x => x.Name == "Create" && x.GetParameters().Count() == types.Length);
+            if (valueTupleCreateMethod == null)
+            {
+                //could happen if there are more than 8 arguments
+                throw new ApplicationException("Cannot deserialize this method's arguments. Try cutting the number of arguments down to 8 or less.");
+            }
+            var genericValueTupleCreateMethod = valueTupleCreateMethod.MakeGenericMethod(types);
+            var dummyValues = new List<object>();
+            foreach (var type in types)
+            {
+                if (type.IsValueType)
+                {
+                    dummyValues.Add(Activator.CreateInstance(type));
 
-				}
-				else
-				{
-					dummyValues.Add(null);
-				}
-			}
-			var valueTuple = genericValueTupleCreateMethod.Invoke(null, dummyValues.ToArray());
-			return valueTuple.GetType();
-		}
+                }
+                else
+                {
+                    dummyValues.Add(null);
+                }
+            }
+            var valueTuple = genericValueTupleCreateMethod.Invoke(null, dummyValues.ToArray());
+            return valueTuple.GetType();
+        }
 
-		public bool ContainsHandler(string routeName)
-		{
-			if (routeName == null)
-			{
-				throw new ArgumentNullException(nameof(routeName));
-			}
-			var key = routeName.Trim().ToLowerInvariant();
-			return _routeHandlers.ContainsKey(key);
-		}
+        public bool ContainsHandler(string routeName)
+        {
+            if (routeName == null)
+            {
+                throw new ArgumentNullException(nameof(routeName));
+            }
+            var key = routeName.Trim().ToLowerInvariant();
+            return _routeHandlers.ContainsKey(key);
+        }
 
-		public void AddHandler(string routeName, Func<string, T, HttpContext, Task<RouteResponse>> handler)
-		{
-			if (routeName == null)
-			{
-				throw new ArgumentNullException(nameof(routeName));
-			}
-			if (handler == null)
-			{
-				throw new ArgumentNullException(nameof(handler));
-			}
+        public void AddHandler(string routeName, Func<string, T, HttpContext, Task<RouteResponse>> handler)
+        {
+            if (routeName == null)
+            {
+                throw new ArgumentNullException(nameof(routeName));
+            }
+            if (handler == null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
 
-			var key = routeName.Trim().ToLowerInvariant();
-			if (_routeHandlers.ContainsKey(key))
-			{
-				throw new ApplicationException(string.Format("A handler for the route \"{0}\" has already been added.", routeName));
-			}
-			_routeHandlers.Add(key, handler);
-		}
+            var key = routeName.Trim().ToLowerInvariant();
+            if (_routeHandlers.ContainsKey(key))
+            {
+                throw new ApplicationException(string.Format("A handler for the route \"{0}\" has already been added.", routeName));
+            }
+            _routeHandlers.Add(key, handler);
+        }
 
-		public Func<string, T, HttpContext, Task<RouteResponse>> GetHandler(string routeName)
-		{
-			if (routeName == null)
-			{
-				throw new ArgumentNullException(nameof(routeName));
-			}
-			var key = routeName.Trim().ToLowerInvariant();
-			return _routeHandlers[key];
-		}
-	}
+        public Func<string, T, HttpContext, Task<RouteResponse>> GetHandler(string routeName)
+        {
+            if (routeName == null)
+            {
+                throw new ArgumentNullException(nameof(routeName));
+            }
+            var key = routeName.Trim().ToLowerInvariant();
+            return _routeHandlers[key];
+        }
+
+        private MethodInfo[] GetAllMethods(Type parentType)
+        {
+            var parentMethods = parentType.GetMethods();
+
+            var typeFilterFunction = new Func<Type, Object, bool>((t, o) =>
+            {
+                // bypass filter and always return true, so we get all.
+                return true;
+            });
+
+            var childInterfaces = parentType.FindInterfaces(new TypeFilter(typeFilterFunction), null);            
+
+            if (childInterfaces != null && childInterfaces.Length > 0)
+            {
+                List<MethodInfo> childInterfaceMethods = new List<MethodInfo>();
+
+                foreach (var childInterface in childInterfaces.Where(o => o != null))
+                {
+                    var methods = childInterface.GetMethods();
+
+                    if (methods != null && methods.Length > 0)
+                    {
+                        childInterfaceMethods.AddRange(methods);
+                    }
+                }
+
+                if (childInterfaceMethods.Count > 0)
+                {
+                    return parentMethods.Union(childInterfaceMethods).ToArray();
+                }
+            }
+
+            return parentMethods;
+        }
+    }
 }
